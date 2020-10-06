577aa9eac2f0e9ceb910f16ba999c9cf01153c84
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-10-06 19:43:39 +0300

    Git Init

---
+++ SimplePlatformGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimplePlatformGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using var game = new PlatformGame();
+            game.Run();
+        }
+    }
+}
