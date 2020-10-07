6a0a0b529537b4eedbe286bf5d7ef42af8590597
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-10-07 10:37:53 +0300

    Create level entity

---
+++ SimplePlatformGame/Level.cs
@@ -0,0 +1,22 @@
+﻿namespace SimplePlatformGame
+{
+    public class Level
+    {
+        public Level(string levelPlan)
+        {
+            // TODO: Add plan parsing
+            
+            // TODO: Initialize fields
+        }
+
+        public void Load()
+        {
+            // TODO: Load content specific for this level
+        }
+
+        public void Dispose()
+        {
+            // TODO: Free up resources
+        }
+    }
+}
