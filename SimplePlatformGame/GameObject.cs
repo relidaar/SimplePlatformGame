dc182736f2b0157cbdf1cfed428b482b0aac1817
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-10-07 13:24:00 +0300

    Add basic game object entity

---
+++ SimplePlatformGame/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SimplePlatformGame
+{
+    public class GameObject : IGameObject
+    {
+        public Vector2 Position { get; }
+        public bool IsStatic { get; }
+
+        public ISprite Sprite { get; }
+        public int Width { get; }
+        public int Height { get; }
+
+        public GameObject(Vector2 position, bool isStatic, ISprite sprite)
+        {
+            Position = position;
+            IsStatic = isStatic;
+            Sprite = sprite;
+            Width = sprite.Width;
+            Height = sprite.Height;
+        }
+        
+        public void Draw(SpriteBatch target)
+        {
+            target.Draw(Sprite.Texture, Position, Color.White);
+        }
+    }
+}
