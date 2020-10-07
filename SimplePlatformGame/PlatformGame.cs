d21dc920d4a619f194a3924f4c3f63b635793533
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-10-07 14:58:43 +0300

    Add level loading and drawing

--- SimplePlatformGame/PlatformGame.cs
+++ SimplePlatformGame/PlatformGame.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,18 +10,21 @@ namespace SimplePlatformGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private IList<Level> _levels;
+        private Level _currentLevel;
+        private string _levelsDirectory;
 
         public PlatformGame()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            _levelsDirectory = "Levels";
+            
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
@@ -27,7 +32,14 @@ namespace SimplePlatformGame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            _levels = new List<Level>();
+            foreach (string filePath in Directory.GetFiles( $"{Content.RootDirectory}/{_levelsDirectory}/"))
+            {
+                _levels.Add(new Level(filePath, _graphics.GraphicsDevice));
+            }
+            
+            _currentLevel = _levels[0];
+            _currentLevel.Load();
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,8 +47,17 @@ namespace SimplePlatformGame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
+            
+            var state = Keyboard.GetState();
+            float timeDelta = (float) gameTime.ElapsedGameTime.TotalSeconds;
+            if (state.IsKeyDown(Keys.Up))
+                _currentLevel.Player.Move(Direction.Up);
+            if(state.IsKeyDown(Keys.Down))
+                _currentLevel.Player.Move(Direction.Down);
+            if (state.IsKeyDown(Keys.Left))
+                _currentLevel.Player.Move(Direction.Left);
+            if(state.IsKeyDown(Keys.Right))
+                _currentLevel.Player.Move(Direction.Right);
 
             base.Update(gameTime);
         }
@@ -45,8 +66,10 @@ namespace SimplePlatformGame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _currentLevel.Draw(_spriteBatch);
+            _spriteBatch.End();
+            
             base.Draw(gameTime);
         }
     }
