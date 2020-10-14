e642aa572a5a1ab42e9ded514ac50b907ab7300e
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-10-14 12:02:31 +0300

    Add collider component

---
+++ SimplePlatformGame/Components/Collider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SimplePlatformGame.Components
+{
+    public class Collider : ICollidable
+    {
+        public Rectangle HitBox { get; set; }
+        public Vector2 Dimensions { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 OldPosition { get; set; }
+        public Vector2 Velocity { get; set; }
+        public bool IsStatic { get; }
+        
+        public bool OnContactBegin(ICollidable collidable, bool fromLeft, bool fromTop)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Collider(Rectangle hitBox,
+            Vector2 dimensions,
+            Vector2 position,
+            Vector2 oldPosition,
+            Vector2 velocity,
+            bool isStatic)
+        {
+            HitBox = hitBox;
+            Dimensions = dimensions;
+            Position = position;
+            OldPosition = oldPosition;
+            Velocity = velocity;
+            IsStatic = isStatic;
+        }
+    }
+}
---
+++ SimplePlatformGame/Components/ICollidable.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SimplePlatformGame.Components
+{
+    public interface ICollidable
+    {
+        Rectangle HitBox { get; set; }
+        Vector2 Dimensions { get; set; }
+        Vector2 Position { get; set; }
+        Vector2 OldPosition { get; set; }
+        Vector2 Velocity { get; set; }
+        bool IsStatic { get; }
+        bool OnContactBegin(ICollidable collidable, bool fromLeft, bool fromTop);
+    }
+}
