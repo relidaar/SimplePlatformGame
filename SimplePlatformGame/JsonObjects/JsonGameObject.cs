23bf17140b15a9633f2b1d83478f8171617dfeab
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-10-14 20:04:57 +0300

    Add entities for json deserialization

---
+++ SimplePlatformGame/JsonObjects/JsonGameObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace SimplePlatformGame.JsonObjects
+{
+    public class JsonGameObject
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public static Dictionary<string, Color> Colors = new Dictionary<string, Color>
+        {
+            {"Black", Color.Black},
+            {"White", Color.White},
+            {"DimGray", Color.DimGray},
+            {"Red", Color.Red},
+        };
+    }
+}
---
+++ SimplePlatformGame/JsonObjects/JsonPlayer.cs
@@ -0,0 +1,8 @@
+﻿namespace SimplePlatformGame.JsonObjects
+{
+    public class JsonPlayer : JsonGameObject
+    {
+        public float RunSpeed { get; set; }
+        public float JumpSpeed { get; set; }
+    }
+}
