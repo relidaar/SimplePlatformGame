5dcc12e0f8493c62e76a72fac4fcf167a9af4ce8
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-10-14 12:04:25 +0300

    Add obstacle as game object

---
+++ SimplePlatformGame/GameObjects/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using SimplePlatformGame.Components;
+
+namespace SimplePlatformGame.GameObjects
+{
+    public class Obstacle : GameObject
+    {
+        public ICollidable Collider { get; }
+        
+        public Obstacle(Vector2 position, ISprite sprite) : base(position, sprite)
+        {
+        }
+
+        public override void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
