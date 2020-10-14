10f57b7c97c617b31dda820cd12b4ecec80a0280
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-10-14 12:01:49 +0300

    Update project structure

--- SimplePlatformGame/PlatformGame.cs
+++ SimplePlatformGame/PlatformGame.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SimplePlatformGame.GameObjects;
 
 namespace SimplePlatformGame
 {
